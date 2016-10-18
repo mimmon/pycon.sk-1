06604d52c4cbddaea8cdf8fd9f468c357f3f373b
Richard Kellner <richard@kellnerovci.sk>
2016-10-18 22:29:03 +0200

    Updated manifest and config

--- doc/staging.py
+++ doc/staging.py
@@ -3,17 +3,17 @@ import gunicorn
 gunicorn.SERVER_SOFTWARE = 'pyconsk/0.2a'
 del gunicorn
 
-proc_name = 'gunicorn-pyconsk'
+proc_name = 'pyconsk-staging'
 umask = 0o022
-bind = '127.0.0.1:8000'
+bind = '127.0.0.1:8008'
 workers = 2
 worker_class = 'sync'
 worker_connections = 100
 debug = False
 daemon = False
 loglevel = 'info'
-accesslog = '/data/www/pycon.sk/var/log/pyconsk.access.log'
-errorlog = '/data/www/pycon.sk/var/log/pyconsk.error.log'
+accesslog = '/data/www/staging.pycon.sk/var/log/pyconsk.access.log'
+errorlog = '/data/www/staging.pycon.sk/var/log/pyconsk.error.log'
 access_log_format = '"%(h)s %({X-FORWARDED-FOR}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 x_forwarded_for_header = 'X-FORWARDED-FOR'
 forwarded_allow_ips = '127.0.0.1'
